6ce83cf741d892eaadb50b0dc79cfb1a8db22db9
Emirhan <emirhan.bda0@gmail.com>
2026-03-21 14:32:31 +0300

    s

--- eticaret-application/src/components/Loading.jsx
+++ eticaret-application/src/components/Loading.jsx
@@ -7,7 +7,7 @@ function Loading() {
  const {loading} = useSelector((store)=> store.product);
   return (
     <Backdrop
-      sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+      sx={{ color: "#fff", zIndex: (theme)=> theme.zIndex.drawer + 1 }}
       open={loading}
     >
       <CircularProgress color="inherit" />
