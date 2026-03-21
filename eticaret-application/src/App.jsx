b2041a680badab82aae5acefbdcd9be151135602
Emirhan <emirhan.bda0@gmail.com>
2026-03-21 11:58:58 +0300

    f

--- eticaret-application/src/App.jsx
+++ eticaret-application/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+ 
+import PageContainer from './container/PageContainer';
+import Header from './components/Header';
+<Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
 
 function App() {
  
 
   return (
     <>
-     
+     <PageContainer>
+      <Header/>
+     </PageContainer>
     </>
   )
 }
